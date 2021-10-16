f8fd59f092466d106e79ab1a12fa62e5093482b5
serhiirodikov <sergiy1998373@gmail.com>
2021-10-16 12:10:27 +0300

    Added images

---
+++ src/container/index.js
@@ -0,0 +1,11 @@
+import { MainHeroSection } from "./MainHeroSection";
+import { HowItSection } from "./HowItSection";
+
+export const OmniFoodMainPage = () => {
+  return (
+    <>
+      <MainHeroSection />;
+      <HowItSection />
+    </>
+  );
+};
